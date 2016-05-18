acabd7bf0ddf2a84859152db05640edc5c46a312
doanhtu <tu0703@gmail.com>
2016-05-18 17:00:26 +0700

    add description under logo

--- conf.py
+++ conf.py
@@ -120,6 +120,9 @@ html_theme = 'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #html_theme_options = {}
+html_theme_options = {
+    "description": "Lecture & Exercise of PyFML Class",
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
