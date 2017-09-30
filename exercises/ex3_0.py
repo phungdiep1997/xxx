9ee1bb3c68750bc069c986656569be0cf466b275
Viet Hung Nguyen <hvn@familug.org>
2017-09-30 12:01:20 +0700

    Explain input is provided

--- exercises/ex3_0.py
+++ exercises/ex3_0.py
@@ -2,7 +2,12 @@
 
 
 def squared(input_data):
-    '''Tính bình phương của số đầu vào'''
+    '''Tính bình phương của số đầu vào
+
+    Số đầu vào ở đây được chứa trong tên `input_data`,
+    được cung cấp sẵn bởi bài tập. Học viên chỉ cần lo tính toán ra kết quả
+    dựa trên `input_data` đã cho.
+    '''
     result = None
 
     # Xoá dòng raise và Viết code vào đây set các giá trị phù hợp
