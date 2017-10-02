ce129dd00c437fb5f60f00001bcf3157f7548786
Viet Hung Nguyen <hvn@familug.org>
2017-10-02 13:37:04 +0700

    Use -, not weird char

--- exercises/ex4_4.py
+++ exercises/ex4_4.py
@@ -7,7 +7,7 @@ def solve():
     Với các biến a,b,c,d,e,f,g,h,i là các số nằm trong khoảng 1-9 (các biến có
     thể có giá trị giống nhau), dạng biểu thức:
 
-      a + 13 * b / c + d + 12 * e – f – 11 + g * h / i – 10 = 66
+      a + 13 * b / c + d + 12 * e - f - 11 + g * h / i - 10 = 66
 
     Bài toán lớp 3 có số đáp án khổng lồ
     (http://www.familug.org/2015/05/codegolf-giai-bai-toan-lop-3-co-so.html)
