b0bfb611e6a8cd14605c5422e6848d10c037f481
Viet Hung Nguyen <hvn@familug.org>
2017-10-02 14:28:13 +0700

    Add note as the special quotes is source of questions

--- exercises/ex5_3.py
+++ exercises/ex5_3.py
@@ -3,6 +3,8 @@
 data = '''Dictionaries are sometimes found in other languages as “associative memories” or “associative arrays”. Unlike sequences, which are indexed by a range of numbers, dictionaries are indexed by keys, which can be any immutable type; strings and numbers can always be keys. Tuples can be used as keys if they contain only strings, numbers, or tuples; if a tuple contains any mutable object either directly or indirectly, it cannot be used as a key. You can’t use lists as keys, since lists can be modified in place using index assignments, slice assignments.''' # NOQA
 
 
+# Chú ý: dấu “ không phải double quotes "
+
 def solve(input_data):
     '''
     Đặt result bằng list chứa 10 tuple ứng với 10 từ xuất hiện nhiều nhất,
