02c71d24955bc275ecd8229f28417b72d0272863
htl <hoangthanhlong@gmail.com>
2018-08-05 23:13:48 +0700

    pep8

--- delete_old_branches.py
+++ delete_old_branches.py
@@ -5,9 +5,7 @@
 Delete old branches. Defaults to > 3 months
 """
 import datetime
-import json
 import os
-
 
 import requests
 
@@ -21,7 +19,7 @@ def delete_old_branches(max_months=3):
     # token = os.environ['GITLAB_TOKEN']
     token = open(os.path.expanduser('~/.config/gitlab')).read().strip()
 
-    branches_url = 'https://gitlab.com/api/v4/projects/1591562/repository/branches' #noqa
+    branches_url = 'https://gitlab.com/api/v4/projects/1591562/repository/branches'  # noqa
     headers = {'Private-Token': token, 'content-type': 'application/json'}
     s = requests.Session()
     branches = []
@@ -33,7 +31,7 @@ def delete_old_branches(max_months=3):
         if repos:
             for repo in repos:
                 if repo['name'] != 'master':
-                    branches.append((repo['name'], repo['commit']['created_at'])) #noqa
+                    branches.append((repo['name'], repo['commit']['created_at']))  # noqa
             page += 1
         else:
             break
@@ -64,8 +62,8 @@ def lambda_handler(*args):
         CiphertextBlob=base64.b64decode(encrypted_gitlab_token)
     )['Plaintext'].decode('ascii')
 
-    class_id = os.environ['class_id']
-    token = gitlab_token
+    # class_id = os.environ['class_id']
+    # token = gitlab_token
     delete_old_branches()
 
 
